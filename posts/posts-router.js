90602fa58b6b265c2b8d9ce69f40259d2c2ce63e
Ben Venker <bvemails@gmail.com>
2020-04-27 21:28:10 -0500

    add POST endpoint to create new posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,28 @@ const express = require("express");
 const posts = require("../data/db");
 const router = express.Router();
 
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  console.log({ title, contents });
+
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+  posts
+    .insert({ title, contents })
+    .then((post) => res.status(201).json({ message: "Post created" }))
+    .catch((err) =>
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database",
+        })
+    );
+});
+
+// GET the posts
 router.get("/", (req, res) => {
   posts
     .find()
