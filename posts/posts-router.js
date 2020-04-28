b51cb39c94918a8924259fe36f2c9f46a3e9ac54
Ben Venker <bvemails@gmail.com>
2020-04-27 23:07:17 -0500

    POST and GET comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const posts = require("../data/db");
+const comments = require("../data/db");
 const router = express.Router();
 
+// Creates a post using the information sent inside the request body
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
   console.log({ title, contents });
@@ -15,15 +17,39 @@ router.post("/", (req, res) => {
     .insert({ title, contents })
     .then((post) => res.status(201).json({ message: "Post created" }))
     .catch((err) =>
-      res
-        .status(500)
-        .json({
-          error: "There was an error while saving the post to the database",
-        })
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      })
     );
 });
 
-// GET the posts
+// Creates a comment for the post with the specified id using information
+// sent inside of the request body
+router.post("/:id/comments", (req, res) => {
+  if (!req.body.text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  }
+  posts
+    .findById(req.params.id)
+    .then((post) => {
+      console.log(post);
+      if (post === undefined)
+        return res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      comments
+        .insertComment({ post_id: req.params.id, text: req.body.text })
+        .then((comment) => {
+          console.log(comment);
+          res.status(201).json(comment);
+        });
+    })
+    .catch((err) => console.log(err));
+});
+
+// GET a list of posts
 router.get("/", (req, res) => {
   posts
     .find()
@@ -35,6 +61,15 @@ router.get("/", (req, res) => {
         message: "Error retrieving the posts",
       });
     });
+});
+
+// GET to /api/posts/:id
+
+// GET to /api/posts/:id/comments
+router.get("/:id/comments", (req, res) => {
+  comments
+    .findPostComments(req.params.id)
+    .then((comments) => res.json(comments));
 });
 
 module.exports = router;
