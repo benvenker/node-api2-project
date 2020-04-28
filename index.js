d576837fb1aad7411450c3a1d57577b0466fe93e
Ben Venker <bvemails@gmail.com>
2020-04-27 21:17:14 -0500

    create posts GET endpoint

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
 const server = express();
+const postsRouter = require("./posts/posts-router");
 
+server.use(express.json());
+server.use("/api/posts", postsRouter);
 server.get("/", (req, res) => {
   res.json({
     message: "Welcome!",
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,3 +1,18 @@
 const express = require("express");
+const posts = require("../data/db");
+const router = express.Router();
 
-const router = express.Router;
+router.get("/", (req, res) => {
+  posts
+    .find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error retrieving the posts",
+      });
+    });
+});
+
+module.exports = router;
